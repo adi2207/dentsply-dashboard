c8f1f2b460d33cf5290c369e6434e2734dcd5d7f
Aditi Pal <aditipal96@gmail.com>
2021-03-10 03:33:01 +0530

    added json file

---
+++ src/Components/Form/form.jsx
@@ -0,0 +1,13 @@
+import './form.css';
+
+import React, { useState } from 'react';
+
+const Form = () => {
+   
+    return(
+       <React.Component>
+
+        </React.Component>
+    )
+}
+export default Form;
